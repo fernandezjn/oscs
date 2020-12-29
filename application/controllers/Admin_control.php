f1d862b0df0705c3f4fc964e496819848f8c7a37
Se-Raf <senados.rafael@gmail.com>
2020-12-29 11:05:23 +0800

    Additional Controller
    for admin, school officials, registrar, and students

---
+++ application/controllers/Admin_control.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin_control extends CI_Controller {
+
+	public function __construct()
+	{
+		//call CodeIgniter's default Constructor
+		parent::__construct();
+		//load database library manually
+		$this->load->database();
+		$this->load->helper('form');
+		$this->load->helper('url');
+    	$this->load->library('form_validation');
+		$this->load->library('session');
+		//load Model
+		$this->load->model('oscs_model');		
+	}
+
+	public function index($id=null)
+	{
+		// ===== Authenticate User permission to the pages =====
+		// if($id!=null)
+		// {
+		// 	$this->session->set_userdata("user_id",$id);
+		// 	$isAdmin = $this->user_model->confirm_user($id,"Admin");
+		// 	$this->session->set_userdata("permission",$isAdmin);
+		// 	if($isAdmin)
+		// 	{
+		// 		redirect("admin/mainPage");
+		// 	}
+		// 	else
+		// 	{
+		// 		redirect("main/mainPage");
+		// 	}
+		// }
+		// else
+		// {
+		// 		redirect("main/mainPage");
+		// }
+		echo "Welcome to Admin Page";
+	}
+
+	public function mainPage()
+	{
+		echo "Welcome to Admin Page";
+		//$this->load->view('');
+
+	}
+
+}
---
+++ application/controllers/Student_control.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Student_control extends CI_Controller 
+{
+
+	public function __construct()
+	{
+		//call CodeIgniter's default Constructor
+		parent::__construct();
+		//load database library manually
+		$this->load->database();
+		$this->load->helper('form');
+		$this->load->helper('url');
+    	$this->load->library('form_validation');
+		$this->load->library('session');
+		//load Model
+		$this->load->model('oscs_model');		
+	}
+
+	public function index($id=null)
+	{
+		// ===== Authenticate User permission to the pages =====
+		// if($id!=null)
+		// {
+		// 	$this->session->set_userdata("user_id",$id);
+		// 	$isAdmin = $this->user_model->confirm_user($id,"Admin");
+		// 	$this->session->set_userdata("permission",$isAdmin);
+		// 	if($isAdmin)
+		// 	{
+		// 		redirect("student/mainPage");
+		// 	}
+		// 	else
+		// 	{
+		// 		redirect("main/mainPage");
+		// 	}
+		// }
+		// else
+		// {
+		// 		redirect("main/mainPage");
+		// }
+		echo "Welcome to Student Page";
+	}
+
+	public function mainPage()
+	{
+		echo "Welcome to Student Page";
+		//$this->load->view('');
+
+	}
+
+}
