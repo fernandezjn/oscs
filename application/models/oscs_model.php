a8f85c0edccb24b44a1da089c345a8fea419ad03
Se-Raf <senados.rafael@gmail.com>
2021-01-20 19:15:02 +0800

    View Edit Delete User Model

--- application/models/oscs_model.php
+++ application/models/oscs_model.php
@@ -62,15 +62,14 @@
 
         public function countAllClearedStudents()
         {
-            $query = "SELECT u.id, COUNT(c.id) as 'number_of_cleared_departments', CONCAT(i.firstname,' ',i.middlename,' ',i.lastname)
+            $query = $this->db->query("SELECT u.id, COUNT(c.id) as 'number_of_cleared_departments', CONCAT(i.firstname,' ',i.middlename,' ',i.lastname)
                         FROM clerance_entries c
                         INNER JOIN user_info i on i.student_number = c.student_number
                         INNER JOIN users u on u.id = i.id
                         WHERE c.deficiencies = 'Clear' AND u.type = '3'
                         GROUP BY u.id
-                        ORDER BY number_of_cleared_departments DESC";
-            $this->db->query($query);
-
+                        ORDER BY number_of_cleared_departments DESC");
+        
             $count = 0;
             foreach($query->result() as $row)
             {
@@ -95,6 +94,63 @@
 
             $this->db->query($query);
         }
+
+        public function viewUsers($role="")
+        {
+            $query = $this->db->query("SELECT u.id, u.username,CONCAT(i.last_name, i.first_name, i.middle_name, i.suffix_name) as 'name', u.type as 'role'
+                FROM user u
+                INNER JOIN user_information i on i.id = u.id
+                WHERE u.type = '".$role."'");
+
+            return $query->result();
+        }
+
+        public function viewUserInfo($id)
+        {
+            $query = $this->db->query("SELECT *
+                FROM user u
+                INNER JOIN user_information i on i.id = u.id
+                WHERE u.id = '".$id."'");
+
+            return $query->result();
+        }
+
+        public function editUserInfo($id,$password="",$lastname="",$firstname="",$middlename="",$suffixname="",$email="",$contactnumber="",$studentno="",$yearid="",$courseid="",$studenttypeid="",$orgid="",$departmentid="", $positionid="")
+        {
+            if($password!="")
+            {
+                 $query = "UPDATE `users` SET `password`='".$password."' WHERE `id`='".$id."'";
+
+                $this->db->query($query);
+            }
+
+            $query = "UPDATE `user_info` 
+                    SET `last_name`='".$lastname."',
+                    `first_name`='".$firstname."',
+                    `middle_name`='".$middlename=""."',
+                    `suffix_name`='".$suffixname."',
+                    `email`='".$email."',
+                    `contact_number`='".$contactnumber."',
+                    `student_number`='".$studentno."',
+                    `year_id`='".$yearid."',
+                    `course_id`='".$courseid."',
+                    `student_type_id`='".$studenttypeid."',
+                    `org_id`='".$orgid."',
+                    `department_id`='".$departmentid."',
+                    `position_id`='".$positionid."' 
+                    WHERE id = '".$id."'";
+
+            $this->db->query($query);
+        }
+
+        public function deleteUser($id)
+        {
+            $query = "DELETE FROM users WHERE id = '".$id."'"
+            $this->db->query($query);
+            $query = "DELETE FROM user_info WHERE id = '".$id."'"
+            $this->db->query($query);
+        }
+
 
         // For Remembering Username and Password Login
         // function rememberUserLogin($user,$password) 
